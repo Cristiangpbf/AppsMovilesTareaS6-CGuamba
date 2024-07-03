112aa27389e1f974175d2e0c98c5b299dd568e85
Master <master@srvuisrael.edu.ec>
2024-07-02 21:54:39 -0500

    Primer commit trabajo en clase

---
+++ Views/vEstudiante.xaml.cs
@@ -0,0 +1,29 @@
+
+using cGuambaS6.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
+
+namespace cGuambaS6.Views;
+
+public partial class vEstudiante : ContentPage
+{
+	private const string urlWS = "http://192.168.17.20/semana6/estudiantews.php";
+	private readonly HttpClient cliente = new HttpClient();
+	private ObservableCollection<Estudiante> estudiantes;
+
+    public vEstudiante()
+	{
+		InitializeComponent();
+		Obtener();
+	}
+
+    private async void Obtener()
+    {
+		var content = await cliente.GetStringAsync(urlWS);
+		List<Estudiante> listaEstudiantes = JsonConvert.DeserializeObject<List<Estudiante>>(content);
+		estudiantes = new ObservableCollection<Estudiante>(listaEstudiantes);
+        lvEstudiantes.ItemsSource = estudiantes;
+
+    }
+}
